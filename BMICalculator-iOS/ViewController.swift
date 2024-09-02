85d24a7a75fbb09664cc0000cd32164642a20e66
Carlos <carloscontreras9522@gmail.com>
2024-09-02 11:29:03 +0200

    Initial Commit

---
+++ BMICalculator-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator-iOS
+//
+//  Created by Mañanas on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
