7de14f0db16e8f529ce008fa1f91a830eb91f95b
Carlos <carloscontreras9522@gmail.com>
2024-09-03 11:17:32 +0200

    Test hellouWorld

--- BMICalculator-iOS/ViewController.swift
+++ BMICalculator-iOS/ViewController.swift
@@ -8,14 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelito: UILabel!
     
-    var name: String = ""
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func escribeHolamundo(_ sender: Any) {
+        
+        labelito.text = "Hola mundo"
+        
+    }
 }
-
